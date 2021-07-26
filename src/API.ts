fc6b0c9088e7fef0ace6abb1250fd67b9bcfd4c5
marco corapi <marco.corapi@outlook.it>
2021-07-26 13:29:00 +0200

    API base

--- src/API.ts
+++ src/API.ts
@@ -1,2 +1,18 @@
+// since difficulty can be many options, we export enum from TS and call it difficulty
+// inside you speficy the constants, so we never will mistype a difficulty 
+export enum Difficulty {
+  EASY = "easy",
+  MEDIUM = "medium",
+  HARD = "hard",
+
+}
+
 // fetching data from API here
-export { }
+export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) => {
+   const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+   // two await, first you await the fetch and then the JSON conversion  
+   const data = await (await fetch(endpoint)).json();
+   console.log(data);
+   
+
+}
