5c1176bcb75fee54c28bf1129ca33e2de33ba206
marco corapi <marco.corapi@outlook.it>
2021-07-26 15:38:09 +0200

    API with shuffle

--- src/API.ts
+++ src/API.ts
@@ -1,3 +1,4 @@
+import { shuffleArray } from "./utils";
 // since difficulty can be many options, we export enum from TS and call it difficulty
 // inside you speficy the constants, so we never will mistype a difficulty 
 export enum Difficulty {
@@ -7,12 +8,37 @@ export enum Difficulty {
 
 }
 
+// the fetchQuizQuestion contains information that we need to put in TS in case we need to use them
+export type Question = {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
+};
+
+// the purpose of this type is to put together the correct answer and the incorect answer, array of strings
+// in this way we add in Question a new key: value pair answers: string[] called QuestionsState
+// we just add the TS not the actual value, the value is added in line 33
+export type QuestionsState = Question & { answers: string[] };
+
 // fetching data from API here
 export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) => {
    const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
    // two await, first you await the fetch and then the JSON conversion  
    const data = await (await fetch(endpoint)).json();
-   console.log(data);
+   console.log(data); // so you know that the result of the data is a JSON called results
+   return data.results.map((question: Question) => {
+     // we want to return an object, so it is easy to manipulate in APP
+     // inside we spread the question array and the answers using the shuffleArray function in utils.js
+     return (
+       {
+         ...question,
+         answers: shuffleArray([...question.incorrect_answers, question.correct_answer]),
+       }
+     )
+   })
    
 
 }
