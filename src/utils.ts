2682e31358669744272a3cf78b82d0e72c043c28
marco corapi <marco.corapi@outlook.it>
2021-07-26 15:31:13 +0200

    shuffle answers

--- src/utils.ts
+++ src/utils.ts
@@ -1,2 +1,5 @@
 // keep small functions to randomize answers from the question the user sees
-export { }
+// here we shuffle the array, the type is any type inside an array
+export const shuffleArray = (array: any[]) =>
+// the array we give/pass to the function will be spreaded
+  [...array].sort(() => Math.random() - 0.5);
